e1839967036b9f1a835aa853b97137bc428af46f
tapanmaru <tapanmaru@yahoo.co.in>
2017-04-14 16:45:51 +0530

    updated the java file for git jenkins integration

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Its working from jenkins");	
+		System.out.println("Yeeeee !!!!!!!!!   Its working from git commit thru Jenkins");	
 	}
 
 }
